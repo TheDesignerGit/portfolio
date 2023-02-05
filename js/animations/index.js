6e561c5c8d912d32b30112cb8f991edd66d5d298
Wandile Ndaba <thedesigner@email.com>
2023-02-05 08:47:12 +0200

    fixed mobile responsive updates

--- js/animations/index.js
+++
@@ -1,5 +0,0 @@
-export { default as animationEnter } from './animationEnter';
-export { default as animationLeave } from './animationLeave';
-export { default as revealProject } from './revealProject';
-export { default as leaveToProject } from './leaveToProject';
-export { default as leaveFromProject } from './leaveFromProject';
